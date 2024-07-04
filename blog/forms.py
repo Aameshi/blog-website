0094260da05cd273619d066c1a5808204f035397
Aameshi <aameshigodara@gmail.com>
2024-07-05 00:57:55 +0530

    eighth commit

--- blog/forms.py
+++ blog/forms.py
@@ -5,3 +5,11 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('title', 'text',)
+
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+class SignUpForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
--- blog/views.py
+++ blog/views.py
@@ -43,3 +43,19 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+from django.contrib.auth import login, authenticate
+from .forms import SignUpForm
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+    else:
+        form = SignUpForm()
+    return render(request, 'blog/signup.html', {'form':form})
