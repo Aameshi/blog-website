0fe53ee2c81d6040ea63e6aeaafad7f3ae084633
Aameshi <aameshigodara@gmail.com>
2024-07-10 00:00:22 +0530

    seventeenth commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,25 +27,6 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'file': {
-            'level': 'ERROR',
-            'class': 'logging.FileHandler',
-            'filename': 'C:/Users/Aameshi Godara/Desktop/Internships/DOTSQUARES/Django/logs/logfile.log',
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['file'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
-    },
-}
-
 # Application definition
 
 INSTALLED_APPS = [
