da7546475bd9f4df4c313766f4e1cc4d66eaf1a1
Aameshi <aameshigodara@gmail.com>
2024-07-11 15:42:12 +0530

    twenty-third commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--4#dl)n)u5o+d68s37r+bm4o*xk@(ah8%9hh!_^1ksxbi=it2^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-
 DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
